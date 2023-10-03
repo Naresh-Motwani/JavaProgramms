4e7962e43786c56c4b32f4c2e51633f5ea2c9d40
naresh <nareshmotwani946@gmail.com>
2023-10-03 15:55:26 +0530

    Added the line.

--- src/Array/P01_OneD_Array.java
+++ src/Array/P01_OneD_Array.java
@@ -4,6 +4,8 @@ public class P01_OneD_Array {
 
 	public static void main(String[] args) {
 
+		System.out.println("Added line to check");
+
 		int[] arr = new int[5];
 		arr[0] = 10;
 		arr[1] = 20;
